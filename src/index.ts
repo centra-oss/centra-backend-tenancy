0205e963420a495d1c22e592745c6b0c96751936
Hayden Young <hayden@hbjy.dev>
2022-08-19 18:35:39 +0100

    feat: add vscode debug config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-console.log('hello');
-
+export function start(addr = "0.0.0.0", port = 3000) {
+  console.log(`Starting on ${addr}:${port}...`);
+}
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { start } from ".";
+
+start("127.0.0.1");
